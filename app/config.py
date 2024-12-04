3077ec139ecb4a3698abe5040761fa5866d69eca
Azriel Wang <azriel.wang@nyu.edu>
2024-12-03 22:57:29 -0500

    update example config with local URL

--- app/config.py
+++
@@ -1,6 +0,0 @@
-class Settings:
-    SECRET_KEY: str = 'secret_key'
-    ALGORITHM: str = 'HS256'
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(1440)
-    DATABASE_URL: str = 'mysql+pymysql://'
-settings = Settings()
